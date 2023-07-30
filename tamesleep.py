0eef86dc1d347b18bc5132428e75faf92a5f68a9
Vanya55 <vanyapogosyan600@gmail.com>
2023-07-30 13:00:42 +0400

    error updates

--- tamesleep.py
+++ tamesleep.py
@@ -1,5 +1,6 @@
 import random
 import time
+import math
 countries={
 "Japan":"Tokyo",
 "Armenia":"Yerevan",
@@ -11,22 +12,24 @@ l = list(countries.items())
 random.shuffle(l)
 wrong_answers = 0
 correct_answers= 0
+timelimit = 10
+flag = False
 for i in l:
-        if wrong_answers == 3:
-            break
-        start = time.time()
-        print("Вопрос :",i[0])
-        user_input = input().title()
-        end = time.time()
-        elapsed_time = end - start
-
-        if elapsed_time > 10:
-            print("Время вышло!")
-            wrong_answers += 1
-        elif user_input == i[1]:
+    if wrong_answers == 3:
+        break
+    start = time.time()
+    print("Вопрос :",i[0])
+    user_input = input("your answer :").title()
+    end = time.time()
+    elapsed_time = math.floor(end - start)
+    if elapsed_time <= timelimit:
+        if user_input == i[1]:
             correct_answers += 1
         else:
             wrong_answers += 1
+    else:
+        print("Время вышло")
+        break
             
 print("Правильных ответов:", correct_answers)
 print("Проигрышей:", wrong_answers)
