d502918714a16bf45b390ede7f43871b6c47b372
Vanya <vanyapogosyan600@gamil.com>
2023-08-04 17:17:38 +0400

    added file

---
+++ hom_func7.py
@@ -0,0 +1,17 @@
+#def descending_order(lis):         #23
+    #newlist = []
+    #for i in lis:
+        #if type(i)==int:
+            #newlist.append(i)
+        #a = sorted(newlist, reverse=True)
+    #return a
+#print(descending_order([5,"good", 2, 3,"hi", 1, 4,"7"]))
+
+
+#def lenwords(lis):                #24
+    #return sorted(lis, key=len, reverse=True)
+
+#print(lenwords(['apples', 'bananas',"a", 'tofu', 'cats', 'tree']))
+
+
+
