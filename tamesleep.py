c2cc693ff2f17a3000548ecfe67a20ce41da1e47
Vanya55 <vanyapogosyan600@gmail.com>
2023-08-04 11:22:00 +0400

    added 10 seconds to enter a response

--- tamesleep.py
+++ tamesleep.py
@@ -1,6 +1,5 @@
 import random
-import time
-import math
+from inputimeout import inputimeout,TimeoutOccurred
 countries={
 "Japan":"Tokyo",
 "Armenia":"Yerevan",
@@ -10,29 +9,19 @@ countries={
 }
 l = list(countries.items())
 random.shuffle(l)
-wrong_answers = 0
-correct_answers= 0
-timelimit = 10
-flag = False
-for i in l:
-    if wrong_answers == 3:
+dic = dict(l)
+count = 0
+for k,v in dic.items():
+    print("Вопрос :",k)
+    try:
+        user_input=inputimeout(prompt="Ответ :",timeout=10)
+        if user_input==v.lower():
+            count+=1
+    except Exception:
+        print("Время Вышло!")
         break
-    start = time.time()
-    print("Вопрос :",i[0])
-    user_input = input("your answer :").title()
-    end = time.time()
-    elapsed_time = math.floor(end - start)
-    if elapsed_time <= timelimit:
-        if user_input == i[1]:
-            correct_answers += 1
-        else:
-            wrong_answers += 1
-    else:
-        print("Время вышло")
-        break
-            
-print("Правильных ответов:", correct_answers)
-print("Проигрышей:", wrong_answers)
+print("Верные ответи",count)
+
             
             
 
